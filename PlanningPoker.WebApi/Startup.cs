3ca727fba3339930b4a2dc5cef88d140039b792b
Elias Jørgensen <eliaszakarias@gmail.com>
2018-10-12 13:44:13 +0200

    Only enable swagger endpoint in development mode

--- PlanningPoker.WebApi/Startup.cs
+++ PlanningPoker.WebApi/Startup.cs
@@ -41,20 +41,14 @@ namespace PlanningPoker.WebApi
             if (env.IsDevelopment())
             {
                 app.UseDeveloperExceptionPage();
+                app.UseSwagger();
+                app.UseSwaggerUI(c => c.SwaggerEndpoint("/swagger/v1/swagger.json", "PlanningPoker API V1"));
             }
             else
             {
                 app.UseHsts();
             }
-
-            // TODO: Should only be enabled in development
-            app.UseSwagger();
-            app.UseSwaggerUI(c =>
-            {
-                c.SwaggerEndpoint("/swagger/v1/swagger.json", "PlanningPoker API V1");
-            });
             
-            /*app.UseHttpsRedirection();*/
             app.UseMvc();
         }
     }
