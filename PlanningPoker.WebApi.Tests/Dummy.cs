a505ccab1befdeba1ffebb6a7e243c817bc12e2a
Elias Jørgensen <eliaszakarias@gmail.com>
2018-10-12 09:22:32 +0200

    Add test project and add test stage to travis.yml

---
+++ PlanningPoker.WebApi.Tests/Dummy.cs
@@ -0,0 +1,14 @@
+using System;
+using Xunit;
+
+namespace PlanningPoker.WebApi.Tests
+{
+    public class Dummy
+    {
+        [Fact]
+        public void One_plus_one_equals_two()
+        {
+            Assert.Equal(2, 1 + 1);
+        }
+    }
+}
