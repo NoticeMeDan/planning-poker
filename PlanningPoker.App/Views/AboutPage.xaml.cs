c396ef7f907bb52bcd7c70c73a898e997a5c0e21
Elias Jørgensen <eliaszakarias@gmail.com>
2018-11-22 12:21:56 +0100

    Use Xamarin.Forms instead of Xamarin Native

---
+++ PlanningPoker.App/Views/AboutPage.xaml.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PlanningPoker.App.Views
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class AboutPage : ContentPage
+    {
+        public AboutPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
