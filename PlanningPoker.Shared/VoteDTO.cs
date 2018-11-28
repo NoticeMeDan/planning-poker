a0b4ff6c366a167421134b1dd0513e1163f6fc77
IMarnild <im.arnild@gmail.com>
2018-11-28 20:27:23 +0100

    added VoteDTO methods

--- PlanningPoker.Shared/VoteDTO.cs
+++ PlanningPoker.Shared/VoteDTO.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +6,10 @@ namespace PlanningPoker.Shared
 {
     class VoteDTO
     {
+        public int Id { get; set; }
+
+        public int UserId { get; set; }
+
+        public int Estimate { get; set; }
     }
 }
