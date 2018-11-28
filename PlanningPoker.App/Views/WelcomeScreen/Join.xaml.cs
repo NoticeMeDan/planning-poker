d94b420d96977f11bf97a969ccc3c3023b211cbe
Vilfred <Vilfredsikker@gmail.com>
2018-11-28 21:27:08 +0100

    Initial setup for Join and Create page inside Welcomescreen

---
+++ PlanningPoker.App/Views/WelcomeScreen/Join.xaml.cs
@@ -0,0 +1,20 @@
+namespace PlanningPoker.App.Views.WelcomeScreen
+{
+    using System;
+    using Xamarin.Forms;
+    using Xamarin.Forms.Xaml;
+
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Join : ContentPage
+    {
+        public Join()
+        {
+            InitializeComponent();
+        }
+
+        private void HandleClicked(object sender, EventArgs e)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
