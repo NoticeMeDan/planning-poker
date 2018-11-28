2716f82424efd789648ab8c794d0ce6b0c40c839
Vilfred <Vilfredsikker@gmail.com>
2018-11-28 21:08:29 +0100

    Initial view working with welcome screen

---
+++ PlanningPoker.App/Views/WelcomeScreen/WelcomeScreen.xaml.cs
@@ -0,0 +1,12 @@
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace PlanningPoker.App.Views.WelcomeScreen {
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class WelcomeScreen : TabbedPage {
+
+        public WelcomeScreen() {
+            InitializeComponent();
+        }
+    }
+}
