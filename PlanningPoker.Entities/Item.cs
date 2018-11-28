dc71038405276982221b53d09840797680446963
Elias Jørgensen <eliaszakarias@gmail.com>
2018-11-28 18:49:19 +0100

    Add title and description to Items. Woops.

--- PlanningPoker.Entities/Item.cs
+++ PlanningPoker.Entities/Item.cs
@@ -1,9 +1,16 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace PlanningPoker.Entities
 {
     using System.Collections.Generic;
 
     public class Item : BaseEntity
     {
+        [Required]
+        public string Title { get; set; }
+
+        public string Description { get; set; }
+
         public ICollection<Round> Rounds { get; set; }
     }
 }
--- PlanningPoker.Entities/Migrations/20181128174742_ItemsFixMigration.Designer.cs
+++ PlanningPoker.Entities/Migrations/20181128174742_ItemsFixMigration.Designer.cs
@@ -3,15 +3,17 @@ using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using PlanningPoker.Entities;
 
 namespace PlanningPoker.Entities.Migrations
 {
     [DbContext(typeof(PlanningPokerContext))]
-    partial class PlanningPokerContextModelSnapshot : ModelSnapshot
+    [Migration("20181128174742_ItemsFixMigration")]
+    partial class ItemsFixMigration
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
