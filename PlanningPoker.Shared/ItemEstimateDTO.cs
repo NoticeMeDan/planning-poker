4f71dcdc8124876e729289cd6d57a98140a97b3b
IMarnild <im.arnild@gmail.com>
2018-11-28 20:07:06 +0100

    added itemEstimateDTO methods

--- PlanningPoker.Shared/ItemEstimateDTO.cs
+++ PlanningPoker.Shared/ItemEstimateDTO.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.Collections.Generic;
 using System.Text;
 
@@ -6,5 +6,8 @@ namespace PlanningPoker.Shared
 {
     class ItemEstimateDTO
     {
+        public int Id { get; set; }
+
+        public string ItemTitle { get; set; }
     }
 }
