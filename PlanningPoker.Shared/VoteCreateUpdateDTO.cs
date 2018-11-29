c69cb069c646b601641e20f8d3d7d32d3f2c03bf
IMarnild <im.arnild@gmail.com>
2018-11-29 13:57:31 +0100

    created VoteCreateUpdateDTO

---
+++ PlanningPoker.Shared/VoteCreateUpdateDTO.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace PlanningPoker.Shared
+{
+    public class VoteCreateUpdateDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public int UserId { get; set; }
+
+        [Required]
+        public int Estimate { get; set; }
+    }
+}
