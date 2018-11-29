29a3d1cc81fdf12d5932275221c28179c88b5231
Elias Jørgensen <eliaszakarias@gmail.com>
2018-11-29 21:11:32 +0100

    Add dummy test, so that the test .dll is generated

--- PlanningPoker.Services.Tests/SessionRepositoryTests.cs
+++ PlanningPoker.Services.Tests/SessionRepositoryTests.cs
@@ -1,10 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace PlanningPoker.Services.Tests
 {
     class SessionRepositoryTests
     {
+        [Fact]
+        public void True_is_true()
+        {
+            Assert.True(true);
+        }
     }
 }
