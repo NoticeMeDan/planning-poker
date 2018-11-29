035e103ab2270ef3dbdfd08822a862560b24a268
IMarnild <im.arnild@gmail.com>
2018-11-29 13:26:20 +0100

    added CRUD to ISummary

--- PlanningPoker.Services/ISummaryRepository.cs
+++ PlanningPoker.Services/ISummaryRepository.cs
@@ -1,10 +1,20 @@
-using System;
-using System.Collections.Generic;
-using System.Text;
+
+using PlanningPoker.Shared;
+using System.Linq;
+using System.Threading.Tasks;
 
 namespace PlanningPoker.Services
 {
     interface ISummaryRepository
     {
+        Task<SummaryDTO> CreateAsync(SummaryDTO summary);
+
+        Task<SummaryDTO> FindAsync(int summaryId);
+
+        IQueryable<SummaryDTO> Read();
+
+        Task<bool> UpdateAsync(SummaryDTO summary);
+
+        Task<bool> DeleteAsync(int summaryId);
     }
 }
