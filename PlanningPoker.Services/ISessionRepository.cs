24888a4f57018fac07adea155960dce51c272546
IMarnild <im.arnild@gmail.com>
2018-11-29 13:20:50 +0100

    added CRUD to ISessionRepo

--- PlanningPoker.Services/ISessionRepository.cs
+++ PlanningPoker.Services/ISessionRepository.cs
@@ -1,12 +1,22 @@
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 using PlanningPoker.Shared;
 
 namespace PlanningPoker.Services
 {
-    interface ISessionRepository
+    public interface ISessionRepository
     {
-        
+        Task<SessionDTO> CreateAsync(SessionDTO session);
+
+        Task<SessionDTO> FindAsync(int sessionId);
+
+        IQueryable<SessionDTO> Read();
+
+        Task<bool> UpdateAsync(SessionDTO session);
+
+        Task<bool> DeleteAsync(int sessionID);
     }
 }
