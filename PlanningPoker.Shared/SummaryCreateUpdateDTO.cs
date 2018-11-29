fa3dacf9f2bd84c1c2c2130db9bf5043dd2d84eb
IMarnild <im.arnild@gmail.com>
2018-11-29 13:52:42 +0100

    created SummaryCreateUpdateDTO

---
+++ PlanningPoker.Shared/SummaryCreateUpdateDTO.cs
@@ -0,0 +1,16 @@
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace PlanningPoker.Shared
+{
+    public class SummaryCreateUpdateDTO
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public ICollection<ItemEstimateDTO> ItemEstimates { get; set; }
+
+        [Required]
+        public int SessionId { get; set; }
+    }
+}
