37d80c0f7ddb9f0cdd1abc960afa5677abb47b06
jaqy <jaqy@panduro.org>
2018-12-02 21:16:22 +0100

    Set up controllers and test files, completed usercontroller + test. still having bug CS1705

---
+++ PlanningPoker.WebApi/Controllers/UsersController.cs
@@ -0,0 +1,69 @@
+namespace PlanningPoker.WebApi.Controllers
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.AspNetCore.Mvc;
+    using Microsoft.EntityFrameworkCore;
+    using PlanningPoker.Services;
+    using PlanningPoker.Shared;
+
+
+    [Route("api/[controller]")]
+    [ApiController]
+    public class UsersController : ControllerBase
+    {
+        private IUserRepository repository;
+
+        public UsersController(IUserRepository repo)
+        {
+            this.repository = repo;
+        }
+
+        // GET api/users
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<UserDTO>>> Get()
+        {
+            return await this.repository.Read().ToListAsync();
+        }
+
+        // GET api/users/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<UserDTO>> Get(int id)
+        {
+            var user = await this.repository.FindAsync(id);
+
+            if (user == null)
+            {
+                return this.NotFound();
+            }
+
+            return user;
+        }
+
+        // POST api/users
+        [HttpPost]
+        public async Task<ActionResult<UserDTO>> Post([FromBody] UserCreateUpdateDTO user)
+        {
+            var created = await this.repository.CreateAsync(user);
+
+            return this.CreatedAtAction(nameof(this.Get), new { created.Id }, created);
+        }
+
+        // PUT api/users/5
+        [HttpPut("{id}")]
+        public async Task<IActionResult> Put(int id, [FromBody] UserCreateUpdateDTO user)
+        {
+            var updated = await this.repository.UpdateAsync(user);
+
+            if (updated)
+            {
+                return this.NoContent();
+            }
+
+            return this.NotFound();
+        }
+    }
+}
