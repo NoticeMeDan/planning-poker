d24a8a540c69f30d47e46cb684d5b47dcbac6852
IMarnild <im.arnild@gmail.com>
2018-12-03 18:15:06 +0100

    Remove all entities from DTOs

--- PlanningPoker.Shared/RoundDTO.cs
+++ PlanningPoker.Shared/RoundDTO.cs
@@ -1,12 +1,11 @@
 namespace PlanningPoker.Shared
 {
-    using PlanningPoker.Entities;
     using System.Collections.Generic;
 
     public class RoundDTO
     {
         public int Id { get; set; }
 
-        public ICollection<Vote> Votes { get; set; }
+        public ICollection<VoteDTO> Votes { get; set; }
     }
 }
