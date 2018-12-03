5c99779d964adaef6f43d6ba9aadf6bce2c1f3ec
IMarnild <im.arnild@gmail.com>
2018-12-03 19:16:48 +0100

    created test class for CollectionHandler

---
+++ PlanningPoker.Services.Tests/CollectionHandlerTests.cs
@@ -0,0 +1,13 @@
+namespace PlanningPoker.Services.Tests
+{
+    using Xunit;
+
+    public class CollectionHandlerTests
+    {
+        [Fact]
+        public void ToItemEntities_returns_Collection_of_entities()
+        {
+
+        }        
+    }
+}
