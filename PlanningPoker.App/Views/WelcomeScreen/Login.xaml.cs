50bdd66c16a1a6de181448d6078209d751d1c791
Vilfred <Vilfredsikker@gmail.com>
2018-12-07 18:20:05 +0100

    Rewrote WelcomeScreen

--- PlanningPoker.App/Views/WelcomeScreen/Login.xaml.cs
+++ PlanningPoker.App/Views/WelcomeScreen/Login.xaml.cs
@@ -1,6 +1,7 @@
 namespace PlanningPoker.App.Views.WelcomeScreen
 {
     using System;
+    using Microsoft.Extensions.DependencyInjection;
     using PlanningPoker.App.ViewModels;
     using Xamarin.Forms;
     using Xamarin.Forms.Xaml;
@@ -8,17 +9,20 @@ namespace PlanningPoker.App.Views.WelcomeScreen
     [XamlCompilation(XamlCompilationOptions.Compile)]
     public partial class Login : ContentPage
     {
-        private readonly LoginViewModel loginViewModel;
+        private readonly LoginViewModel viewModel;
 
         public Login()
         {
-            this.loginViewModel = new LoginViewModel();
+            this.viewModel = new LoginViewModel();
+            this.BindingContext = this.viewModel =
+                (Application.Current as App)?.Container.GetRequiredService<LoginViewModel>();
+
             this.InitializeComponent();
         }
 
         private void HandleLoginClicked(object sender, EventArgs e)
         {
-            this.loginViewModel.ExecuteLoginCommand();
+            this.viewModel.ExecuteLoginCommand();
             this.Navigation.PushAsync(new SessionCreation.CreateSession());
         }
     }
