0b0397caba3a3e510f1fe92ba8fdc4983165669d
Crunchyalex <Crunchyalex@gmail.com>
2018-12-11 13:51:06 +0100

    Created SessionViewModel

---
+++ PlanningPoker.App/ViewModels/SessionViewModel.cs
@@ -0,0 +1,15 @@
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace PlanningPoker.App.ViewModels
+{
+    class SessionViewModel
+    {
+        internal void Estimate(string estimate)
+        {
+            //This should send a message to the API
+            throw new NotImplementedException();
+        }
+    }
+}
--- PlanningPoker.App/Views/Session/Session.xaml.cs
+++ PlanningPoker.App/Views/Session/Session.xaml.cs
@@ -1,12 +1,20 @@
 namespace PlanningPoker.App.Views.Session
 {
+    using Microsoft.Extensions.DependencyInjection;
+    using PlanningPoker.App.ViewModels;
     using System;
     using Xamarin.Forms;
 
     public partial class Session : ContentPage
     {
+        private readonly SessionViewModel ViewModel;
+
         public Session()
         {
+            this.ViewModel = new SessionViewModel();
+            this.BindingContext = this.ViewModel =
+                (Application.Current as App)?.Container.GetRequiredService<SessionViewModel>();
+
             this.InitializeComponent();
         }
 
