606ae06def6dca43c57b510e8d282761024d5bb5
Oliverjuhl <olju@itu.dk>
2018-12-14 12:30:07 +0100

    Sessionrepository done + Summaryrepositoyry done

--- PlanningPoker.App/Models/SessionRepository.cs
+++ PlanningPoker.App/Models/SessionRepository.cs
@@ -8,43 +8,108 @@ namespace PlanningPoker.App.Models
     public class SessionRepository : ISessionRepository
     {
         private readonly HttpClient httpClient;
+        private readonly UserRepository userRepository;
 
-        public SessionRepository(HttpClient httpClient)
+        public SessionRepository(HttpClient httpClient, UserRepository userRepository)
         {
             this.httpClient = httpClient;
+            this.userRepository = userRepository;
         }
 
         public async Task<SessionDTO> CreateAsync(SessionCreateUpdateDTO session)
         {
-            var response = await this.httpClient.PostAsJsonAsync("api/sessions", session);
+            var response = await this.httpClient.PostAsJsonAsync("api/session", session);
 
             return await response.Content.ReadAsAsync<SessionDTO>();
         }
 
         public async Task<SessionDTO> FindAsync(int sessionId)
         {
-            var response = await this.httpClient.GetAsync($"api/sessions/{sessionId}");
+            var response = await this.httpClient.GetAsync($"api/session/{sessionId}");
 
             return await response.Content.ReadAsAsync<SessionDTO>();
         }
 
         public async Task<IEnumerable<SessionDTO>> ReadAsync()
         {
-            var response = await this.httpClient.GetAsync("api/sessions");
+            var response = await this.httpClient.GetAsync("api/session");
 
             return await response.Content.ReadAsAsync<IEnumerable<SessionDTO>>();
         }
 
         public async Task<bool> UpdateAsync(SessionCreateUpdateDTO session)
         {
-            var response = await this.httpClient.PutAsJsonAsync($"api/sessions/{session.Id}", session);
+            var response = await this.httpClient.PutAsJsonAsync($"api/session/{session.Id}", session);
 
             return response.IsSuccessStatusCode;
         }
 
-        public async Task<bool> DeleteAsync(int sessionId)
+        public async Task<SessionDTO> GetByKeyAsync(string sessionKey)
         {
-            var response = await this.httpClient.DeleteAsync($"api/sessions/{sessionId}");
+            var reponse = await this.httpClient.GetAsync($"api/session/{sessionKey}");
+
+            return await reponse.Content.ReadAsAsync<SessionDTO>();
+        }
+
+        public async Task<UserStateResponseDTO> Join(string sessionKey, UserCreateDTO user)
+        {
+            var reponse = await this.httpClient.PostAsJsonAsync($"api/session/{sessionKey}/join", user);
+
+            return await reponse.Content.ReadAsAsync<UserStateResponseDTO>();
+        }
+
+        public async Task<RoundDTO> NextRoundAsync(string sessionKey)
+        {
+            var response = await this.httpClient.GetAsync($"api/session/{sessionKey}/items/rounds/next");
+
+            return await response.Content.ReadAsAsync<RoundDTO>();
+        }
+
+        public async Task<RoundDTO> GetCurrentRound(string sessionKey)
+        {
+            var response = await this.httpClient.GetAsync($"api/session/{sessionKey}/items/rounds/current");
+
+            return await response.Content.ReadAsAsync<RoundDTO>();
+        }
+
+        public async Task<ItemDTO> NextItemAsync(string sessionKey)
+        {
+            var response = await this.httpClient.GetAsync($"api/session/{sessionKey}/items/next");
+
+            return await response.Content.ReadAsAsync<ItemDTO>();
+        }
+
+        public async Task<ICollection<ItemDTO>> GetAllItems(string sessionKey)
+        {
+            var response = await this.httpClient.GetAsync($"api/session/{sessionKey}/items");
+
+            return await response.Content.ReadAsAsync<ICollection<ItemDTO>>();
+        }
+
+        public async Task<ItemDTO> GetCurrentItem(string sessionKey)
+        {
+            var response = await this.httpClient.GetAsync($"api/session/{sessionKey}/items/current");
+
+            return await response.Content.ReadAsAsync<ItemDTO>();
+        }
+
+        public async Task<bool> Vote(string sessionKey, VoteDTO vote)
+        {
+            var response = await this.httpClient.PostAsJsonAsync($"api/session/{sessionKey}/items/rounds/votes", vote);
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> ThrowNitpickerCard(string sessionKey)
+        {
+            var response = await this.httpClient.PostAsync($"api/session/{sessionKey}/nitpicker", default (HttpContent));
+
+            return response.IsSuccessStatusCode;
+        }
+
+        public async Task<bool> KickUser(string sessionKey, int userId)
+        {
+            var response = await this.httpClient.PostAsJsonAsync($"api/session/{sessionKey}", userId);
 
             return response.IsSuccessStatusCode;
         }
