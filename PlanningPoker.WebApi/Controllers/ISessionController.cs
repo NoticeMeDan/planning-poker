1866a9e0485c271aaa2d4592bd1166d934ef85e8
Oliverjuhl <olju@itu.dk>
2018-12-15 17:16:15 +0100

    fixes

---
+++ PlanningPoker.WebApi/Controllers/ISessionController.cs
@@ -0,0 +1,32 @@
+namespace PlanningPoker.WebApi.Controllers
+{
+    using System.Collections.Generic;
+    using System.Threading.Tasks;
+    using Microsoft.AspNetCore.Mvc;
+    using Shared;
+
+    public interface ISessionController
+    {
+        Task<ActionResult<SessionDTO>> GetByKey(string sessionKey);
+
+        Task<ActionResult<SessionDTO>> Create(SessionCreateUpdateDTO session);
+
+        Task<ActionResult<UserStateResponseDTO>> Join(string sessionKey, UserCreateDTO user);
+
+        Task<ActionResult<RoundDTO>> NextRound(string authToken, string sessionKey);
+
+        Task<ActionResult<RoundDTO>> GetCurrentRound(string authToken, string sessionKey);
+
+        Task<ActionResult<ItemDTO>> NextItem(string authToken, string sessionKey);
+
+        Task<ActionResult<ICollection<ItemDTO>>> GetAllItems(string authToken, string sessionKey);
+
+        Task<ActionResult<ItemDTO>> GetCurrentItem(string authToken, string sessionKey);
+
+        Task<ActionResult> Vote(string authToken, string sessionKey, VoteDTO vote);
+
+        Task<ActionResult> ThrowNitpickerCard(string authToken, string sessionKey);
+
+        Task<ActionResult> KickUser(string authToken, string sessionKey, int userId);
+    }
+}
