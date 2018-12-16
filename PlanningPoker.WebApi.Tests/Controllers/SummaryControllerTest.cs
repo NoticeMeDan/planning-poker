f9ca5497c082b81d6a6e1a8f1391b4b054235d2b
Oliverjuhl <olju@itu.dk>
2018-12-16 15:30:50 +0100

    Modified SummaryControllerTests

--- PlanningPoker.WebApi.Tests/Controllers/SummaryControllerTest.cs
+++ PlanningPoker.WebApi.Tests/Controllers/SummaryControllerTest.cs
@@ -1,30 +1,28 @@
 namespace PlanningPoker.WebApi.Tests.Controllers
 {
-    using System.Linq;
     using System.Threading.Tasks;
     using Microsoft.AspNetCore.Mvc;
-    using MockQueryable.Moq;
     using Moq;
-    using PlanningPoker.Services;
-    using PlanningPoker.Shared;
     using PlanningPoker.WebApi.Controllers;
+    using Services;
+    using Shared;
     using Xunit;
 
     public class SummaryControllerTest
     {
         [Fact]
-        public async Task Get_given_existing_id_returns_dto()
+        public async Task FindBySessionIdAsync_given_existing_id_returns_dto()
         {
             var dto = new SummaryDTO();
             var repository = new Mock<ISummaryRepository>();
-            repository.Setup(s => s.FindAsync(42)).ReturnsAsync(dto);
+            repository.Setup(s => s.FindBySessionIdAsync(42)).ReturnsAsync(dto);
             var controller = new SummaryController(repository.Object);
             var get = await controller.FindBySessionIdAsync(42);
             Assert.Equal(dto, get.Value);
         }
 
         [Fact]
-        public async Task Get_given_non_existing_id_returns_NotFound()
+        public async Task FindBySessionIdAsync_given_non_existing_id_returns_NotFound()
         {
             var repository = new Mock<ISummaryRepository>();
             var controller = new SummaryController(repository.Object);
