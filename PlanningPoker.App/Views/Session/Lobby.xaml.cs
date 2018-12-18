c9844cdbd429a1767437443d844e37abb36961fa
alol <alol@itu.dk>
2018-12-18 13:02:48 +0100

    Stopped jobscheduler on finished

--- PlanningPoker.App/Views/Session/Lobby.xaml.cs
+++ PlanningPoker.App/Views/Session/Lobby.xaml.cs
@@ -3,13 +3,15 @@ namespace PlanningPoker.App.Views.Session
     using System;
     using System.Diagnostics;
     using Microsoft.Extensions.DependencyInjection;
+    using OpenJobScheduler;
     using PlanningPoker.App.ViewModels;
     using Xamarin.Forms;
 
     public partial class Lobby : ContentPage
     {
         private LobbyViewModel viewModel;
-
+        private JobScheduler jobScheduler;
+        
         public Lobby(string sessionKey)
         {
             this.InitializeComponent();
@@ -20,18 +22,27 @@ namespace PlanningPoker.App.Views.Session
             this.viewModel.Key = sessionKey;
             this.viewModel.Title = sessionKey;
             Debug.Write("SessionKey vm: " + this.viewModel.Key);
+
+            this.jobScheduler = new JobScheduler(TimeSpan.FromSeconds(5), new Action(() => { if (this.viewModel.Started) { this.StartSession(); }; }));
+            this.jobScheduler.Start();
         }
 
         private void BeginSessionClicked(object sender, EventArgs e)
         {
-            this.viewModel.StopFetchingUsers.Execute(null);
-            this.Navigation.PushModalAsync(new Session());
+            this.StartSession();
         }
 
         protected override void OnAppearing()
         {
             this.viewModel.GetUsersCommand.Execute(null);
             base.OnAppearing();
+        }
+
+        private void StartSession()
+        {
+            this.jobScheduler.Stop();
+            this.viewModel.StopFetchingUsers.Execute(null);
+            this.Navigation.PushModalAsync(new Session());
         }
     }
 }
