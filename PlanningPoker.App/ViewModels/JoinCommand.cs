19128575570202b143e89a14809a4d0ab79d3ef6
IMarnild <im.arnild@gmail.com>
2018-12-18 10:12:23 +0100

    Refractored JoinCommand, now an individual class..

---
+++ PlanningPoker.App/ViewModels/JoinCommand.cs
@@ -0,0 +1,58 @@
+namespace PlanningPoker.App.ViewModels
+{
+    using System;
+    using System.Diagnostics;
+    using System.Threading.Tasks;
+    using System.Windows.Input;
+    using PlanningPoker.App.Models;
+    using PlanningPoker.Shared;
+
+    public class JoinCommand
+    {
+
+        private readonly UserCreateDTO user;
+
+        private readonly ISessionClient client;
+
+        public ICommand Join { get; }
+
+        public string Key { get; set; }
+
+        public JoinCommand(ISessionClient client, string key, UserCreateDTO user)
+        {
+            this.Key = key;
+            this.user = user;
+            this.client = client;
+            this.Join = new RelayCommand(async _ => await this.ExecuteJoinCommand());
+        }
+
+        public bool Loading { get; private set; }
+
+        private async Task ExecuteJoinCommand()
+        {
+            if (this.Loading)
+            {
+                return;
+            }
+
+            this.Loading = true;
+
+            await this.JoinSession();
+
+            this.Loading = false;
+        }
+
+        private async Task JoinSession()
+        {
+            try
+            {
+                await this.client.Join(this.Key, this.user);
+            }
+            catch (Exception e)
+            {
+                this.Key = string.Empty;
+                Debug.WriteLine("JoinSession failed. Caught exception: " + e.GetType());
+            }
+        }
+    }
+}
---
+++ PlanningPoker.App/ViewModels/SessionCreateViewModel.cs
@@ -0,0 +1,119 @@
+namespace PlanningPoker.App.ViewModels
+{
+    using System.Collections.ObjectModel;
+    using System.Diagnostics;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Windows.Input;
+    using Models;
+    using Shared;
+    using Xamarin.Forms;
+
+    // This class contains data until repositories is setup
+    public class SessionCreateViewModel : BaseViewModel
+    {
+        private readonly ISessionClient sessionRepo;
+        private string title;
+        private string description;
+
+        public SessionCreateViewModel(ISessionClient sessionRepo)
+        {
+            this.sessionRepo = sessionRepo;
+
+            this.BaseTitle = "Items";
+
+            this.Items = new ObservableCollection<ItemCreateUpdateDTO>();
+
+            this.AddItemCommand = new RelayCommand(_ => this.ExecuteAddItemCommand());
+            this.LoadCommand = new Command(() => this.ExecuteLoadCommand());
+            this.CreateSessionCommand = new RelayCommand(async _ => await this.ExecuteCreateSessionCommand());
+        }
+
+        public ObservableCollection<ItemCreateUpdateDTO> Items { get; set; }
+
+        public ICommand AddItemCommand { get; }
+
+        public ICommand CreateSessionCommand { get; }
+
+        public ICommand LoadCommand { get; }
+
+        public string Title
+        {
+            get => this.title;
+            set => this.SetProperty(ref this.title, value);
+        }
+
+        public string Description
+        {
+            get => this.description;
+            set => this.SetProperty(ref this.description, value);
+        }
+
+        public string Key { get; private set; }
+
+        public async Task ExecuteCreateSessionCommand()
+        {
+            if (this.IsBusy)
+            {
+                return;
+            }
+
+            this.IsBusy = true;
+
+            var toCreate = new SessionCreateUpdateDTO
+            {
+                Items = this.Items.ToList()
+            };
+
+            var result = await this.sessionRepo.CreateAsync(toCreate);
+            this.Key = result.SessionKey;
+            this.IsBusy = false;
+        }
+
+        private void ExecuteAddItemCommand()
+        {
+            if (this.IsBusy)
+            {
+                return;
+            }
+
+            this.IsBusy = true;
+
+            var toCreate = new ItemCreateUpdateDTO
+            {
+                Title = this.Title,
+                Description = this.Description
+            };
+
+            this.Items.Add(toCreate);
+
+            MessagingCenter.Send(this, "ItemAdded", toCreate);
+
+            this.Title = string.Empty;
+            this.Description = string.Empty;
+
+            this.IsBusy = false;
+        }
+
+        private void ExecuteLoadCommand()
+        {
+            if (this.IsBusy)
+            {
+                return;
+            }
+
+            this.IsBusy = true;
+
+            this.Items.Clear();
+
+            var items = this.Items.ToList();
+
+            foreach (var item in items)
+            {
+                this.Items.Add(item);
+            }
+
+            this.IsBusy = false;
+        }
+    }
+}
